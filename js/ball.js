34530ff8a1498b5e47d4d38cd31cc41fb6e4a18d
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2014-03-02 13:09:05 -0500

    mobing Ball to be an object

---
+++ js/ball.js
@@ -0,0 +1,63 @@
+// Define a class like this
+function Ball( x, y){
+
+  this.x = x ;
+  this.y = y ;
+  
+  this.radius = 15 ;
+  this.color = "red" ;
+  
+  // Velocity components
+  this.vx= 1;
+  this.vy= 1;
+  
+}
+
+// Add methods like this.  All Person objects will be able to invoke this
+Ball.prototype.draw = function(){
+
+    // reseize  canvas to be full page
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    // here we now draw the associated shadow
+    shadow_scale = this.radius/(1+(H - this.radius -this.y)/100) * 1.8
+    drawEllipseByCenter(ctx,this.x, H, shadow_scale , shadow_scale*0.6 )
+    // ctx.beginPath();
+    // ctx.arc(this.x, H, this.radius/(1+(H - this.radius -this.y)/100) , 0, Math.PI*2, false);
+    // ctx.fillStyle = "grey";
+    // ctx.fill();
+    // ctx.closePath();
+
+    // Here, we'll first begin drawing the path and then use the arc() function to draw the circle. The arc function accepts 6 parameters, x position, y position, radius, start angle, end angle and a boolean for anti-clockwise direction.
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+Ball.prototype.move = function(){
+   // Now, lets make the ball move by adding the velocity vectors to its position
+    this.y += this.vy;
+    this.x += this.vx;
+
+    // Ohh! The this is moving!
+    // Lets add some acceleration
+    this.vy += gravity;
+
+    //Perfect! Now, lets make it rebound when it touches the floor
+    if(this.y + this.radius > H) {
+      // First, reposition the this on top of the floor and then bounce it!
+      this.y = H - this.radius;
+      this.vy *= -bounceFactor;
+      // The bounceFactor variable that we created decides the elasticity or how elastic the collision will be. If it's 1, then the collision will be perfectly elastic. If 0, then it will be inelastic.
+    }
+
+    // Finally check if the this exits the canvas
+    if (this.x > ctx.canvas.width) {
+      this.x = 0;
+      this.y = 0; 
+    }    
+}
--- js/main.js
+++ js/main.js
@@ -41,13 +41,48 @@ ball = {
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
 
+    // here we now draw the associated shadow
+    shadow_scale = this.radius/(1+(H - this.radius -this.y)/100) * 1.8
+    drawEllipseByCenter(ctx,this.x, H, shadow_scale , shadow_scale*0.6 )
+    // ctx.beginPath();
+    // ctx.arc(this.x, H, this.radius/(1+(H - this.radius -this.y)/100) , 0, Math.PI*2, false);
+    // ctx.fillStyle = "grey";
+    // ctx.fill();
+    // ctx.closePath();
+
     // Here, we'll first begin drawing the path and then use the arc() function to draw the circle. The arc function accepts 6 parameters, x position, y position, radius, start angle, end angle and a boolean for anti-clockwise direction.
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
+
+  },
+
+  move: function() {
+    // Now, lets make the ball move by adding the velocity vectors to its position
+    this.y += this.vy;
+    this.x += this.vx;
+
+    // Ohh! The this is moving!
+    // Lets add some acceleration
+    this.vy += gravity;
+
+    //Perfect! Now, lets make it rebound when it touches the floor
+    if(this.y + this.radius > H) {
+      // First, reposition the this on top of the floor and then bounce it!
+      this.y = H - this.radius;
+      this.vy *= -bounceFactor;
+      // The bounceFactor variable that we created decides the elasticity or how elastic the collision will be. If it's 1, then the collision will be perfectly elastic. If 0, then it will be inelastic.
+    }
+
+    // Finally check if the this exits the canvas
+    if (this.x > ctx.canvas.width) {
+      this.x = 0;
+      this.y = 0; 
+    }    
   }
+
 };
 
 // When we do animations in canvas, we have to repaint the whole canvas in each frame. Either clear the whole area or paint it with some color. This helps in keeping the area clean without any repetition mess.
@@ -60,27 +95,9 @@ function clearCanvas() {
 function update() {
   clearCanvas();
   ball.draw();
+  ball.move();
   
-  // Now, lets make the ball move by adding the velocity vectors to its position
-  ball.y += ball.vy;
-  ball.x += ball.vx;
 
-  // Ohh! The ball is moving!
-  // Lets add some acceleration
-  ball.vy += gravity;
-
-  //Perfect! Now, lets make it rebound when it touches the floor
-  if(ball.y + ball.radius > H) {
-    // First, reposition the ball on top of the floor and then bounce it!
-    ball.y = H - ball.radius;
-    ball.vy *= -bounceFactor;
-    // The bounceFactor variable that we created decides the elasticity or how elastic the collision will be. If it's 1, then the collision will be perfectly elastic. If 0, then it will be inelastic.
-  }
-
-  // Finally check if the ball exits the canvas
-  if (ball.x > ctx.canvas.width) {
-    ball.x = 0;
-  }
 }
 
 // Now, the animation time!
